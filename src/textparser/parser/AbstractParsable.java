0e484b9bf37671396257c9bad1c65e34d3a339be
JackKarichkovskiy <karichkovsky@yandex.com>
2015-11-24 13:49:55 +0200

    added AbstractParsable realization

--- src/textparser/parser/AbstractParsable.java
+++ src/textparser/parser/AbstractParsable.java
@@ -5,10 +5,58 @@
  */
 package textparser.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import textparser.utils.TextParserUtils;
+import textparser.word.Word;
+
 /**
  *
  * @author Karichkovskiy Yevhen
  */
-public class AbstractParsable {
-    
+public abstract class AbstractParsable implements Parsable {
+
+    protected String originStr;
+
+    protected final List<AbstractParsable> parsedList = new ArrayList<>();
+
+    public AbstractParsable(String originStr) {
+        this.originStr = TextParserUtils.checkNotNull(originStr);
+    }
+
+    @Override
+    public String compose() {
+        if (parsedList.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder strBuf = new StringBuilder();
+        parsedList.stream().forEach((parseObj) -> {
+            strBuf.append(parseObj.compose());
+        });
+
+        return strBuf.toString();
+    }
+
+    @Override
+    public List<Word> getWords() {
+        List<Word> result = new ArrayList<>();
+
+        for (Parsable parsedPart : parsedList) {
+            result.addAll(parsedPart.getWords());
+        }
+
+        return result;
+    }
+
+    @Override
+    public void pollOrderedWords(Queue<Word> queue) {
+        queue = TextParserUtils.checkNotNull(queue);
+
+        for (Parsable sentence : parsedList) {
+            sentence.pollOrderedWords(queue);
+        }
+    }
+
 }
